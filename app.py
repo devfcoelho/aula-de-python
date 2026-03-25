d7c1b843f02c7080774176e7c03dccc74dc15703
devfcoelho <dafonsecafelipecoelho@gmail.com>
2026-03-25 20:04:35 -0300

    Praticando if else

---
+++ app.py
@@ -0,0 +1,11 @@
+idade = int(input("Digite sua idade: "))
+
+if(idade>=18):
+    print("maior de idade")
+else:
+    print("menor de idade")
+if (idade<18):
+    print("Menor de idade")
+else:
+    print("Maior de idade")
+    
